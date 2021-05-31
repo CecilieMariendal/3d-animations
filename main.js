355af50de8a6d5433ce5cad2256f34c2eddea634
Cecilie Mariendal <ctmariendal@gmail.com>
2021-05-31 21:40:48 +0200

    Add helpers and controls

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -24,7 +25,13 @@ const pointLight = new THREE.PointLight('0xFFFFFF');
 pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight('0xFFFFFF');
-scene.add(pointLight, ambientLight)
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
 function animate() {
